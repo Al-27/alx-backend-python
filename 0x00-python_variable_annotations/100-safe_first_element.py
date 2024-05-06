fd6de287fc34d6ac72c109894a7d986e0f7c9d80
Al_27 <ahmedlaghfour@gmail.com>
2024-05-06 21:16:49 +0100

    fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,8 +5,8 @@ documentation
 import typing
 
 
-def safe_first_element(lst: typing.Sequence[typing.Any]) \
--> typing.Union[typing.Any, None]:
+def safe_first_element(
+        lst: typing.Sequence[typing.Any]) -> typing.Union[typing.Any, None]:
     """
     documentation
     """
