0705ed1587b5fcbae3a83e72be483776787d9991
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-05-11 01:55:27 +0000

    fix func name

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_time() -> float:
+async def measure_runtime() -> float:
     """
     func
     """
