a62ee54a0e781f3ae952cd6fe8c69323decb252e
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-05-11 01:52:49 +0000

    0-2

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ import typing
 from random import uniform
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """
     func
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield uniform(0,10)
+        yield uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,15 @@ documentation
 
 """
 import asyncio
+import typing
 from random import uniform
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
+    """
+    async comp
+    """
     result = []
     async for i in async_generator():
         result.append(i)
